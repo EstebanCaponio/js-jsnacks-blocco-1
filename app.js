b36ebb533bc18a4344d65f9e837f9a73d9d76f79
EstebanCaponio <estebancaponio@gmail.com>
2025-02-27 12:43:20 +0100

    feat: ex 1-4

---
+++ app.js
@@ -0,0 +1,45 @@
+// esercizio 1
+let firstNumber = prompt('Inserisci il primo numero');
+let secondNumber = prompt('Inserisci il secondo numero');
+
+if (firstNumber > secondNumber) {
+    console.log(firstNumber);
+} else
+    console.log(secondNumber);
+console.log('---');
+
+// esercizio 2
+let firstWord = prompt('Inserisci la prima parola');
+let secondWord = prompt('Inserisci la seconda parola');
+
+if (firstWord.length < secondWord.length) {
+    console.log(firstWord);
+    console.log(secondWord);
+} else {
+    console.log(secondWord);
+    console.log(firstWord);
+}
+console.log('---');
+
+// esercizio 3
+let somma = 0;
+for (let i = 1; i <= 10; i++) {
+    let numberUser = prompt('Inserisci il primo numero');
+    let number = parseFloat(numberUser);
+    somma = somma + number2;
+}
+console.log(somma);
+console.log('---');
+
+// esercizio 4
+
+let dispari = [];
+
+for (i = 0; i < 6; i++) {
+    let yourNumber = prompt('Inserisci il primo numero');
+    let myNumber = parseFloat(yourNumber);
+    if (!(myNumber % 2 === 0)1) {
+        dispari.push(myNumber);
+    }
+}
+console.log(dispari);
